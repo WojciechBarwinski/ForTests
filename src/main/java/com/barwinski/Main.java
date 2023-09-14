8c0bd77a1fd45e8eeeca74e78dfe0f4ab4445f1f
Wojtek B. <wojciech.barwinski@gmail.com>
2023-09-14 12:12:56 +0200

    after fetch and merge in intelij

--- src/main/java/com/barwinski/Main.java
+++ src/main/java/com/barwinski/Main.java
@@ -3,6 +3,7 @@ package com.barwinski;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from repository");
+        System.out.println("Hello from local");
         System.out.println("Hello world!");
         
     }
