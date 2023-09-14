76347fec15640dd538deb7649882cfab3d41d980
Wojciech Barwiński <53368019+WojciechBarwinski@users.noreply.github.com>
2023-09-14 12:18:42 +0200

    change texts to create conflict

--- src/main/java/com/barwinski/Main.java
+++ src/main/java/com/barwinski/Main.java
@@ -2,8 +2,9 @@ package com.barwinski;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from repository");
-        System.out.println("Hello from local");
+        System.out.println("Hello z repository");
+        System.out.println("czejsc from local");
         System.out.println("Hello world!");
+        System.out.println("blabla");
     }
 }
