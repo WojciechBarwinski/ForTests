64b09b962371fa6e66462378f676c5ba932fc2a4
Wojtek B. <wojciech.barwinski@gmail.com>
2023-09-14 12:16:44 +0200

    preper for next testing with conflicts

--- src/main/java/com/barwinski/Main.java
+++ src/main/java/com/barwinski/Main.java
@@ -2,10 +2,8 @@ package com.barwinski;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from blabla");
-        System.out.println("Hello aaaaa local");
-        System.out.println("Hello world!");
-
+        System.out.println("Hello from repository");
+        System.out.println("Hello from local");
         System.out.println("Hello world!");
     }
 }
